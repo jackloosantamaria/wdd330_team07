69b6f74ec0549ba2a716bfdccd39b35e792bcfca
noahruppe <noah.ruppe30@gmail.com>
2024-05-02 20:40:28 -0400

    team activity two

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,23 +1,42 @@
-function convertToJson(res) {
-  if (res.ok) {
+// function convertToJson(res) {
+//   if (res.ok) {
+//     return res.json();
+//   } else {
+//     throw new Error("Bad Response");
+//   }
+// }
+
+// export default class ProductData {
+//   constructor(category) {
+//     this.category = category;
+//     this.path = `../json/${this.category}.json`;
+//   }
+//   getData() {
+//     return fetch(this.path)
+//       .then(convertToJson)
+//       .then((data) => data);
+//   }
+//   async findProductById(id) {
+//     const products = await this.getData();
+//     return products.find((item) => item.Id === id);
+//   }
+// }
+
+function convertToJson(res){
+  if (res.ok){
     return res.json();
-  } else {
+  }else{
     throw new Error("Bad Response");
   }
 }
 
-export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `../json/${this.category}.json`;
-  }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
-  }
-  async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
-  }
+export function getData(category = "tents"){
+  return fetch(`../json/${category}.json`)
+    .then(convertToJson)
+    .then((data) => data);
+}
+
+export async function findProductById(id){
+  const products = await getData();
+  return products.find((item) => item.Id === id);
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,4 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
+import ProductData from ".ProductData.mjs";
 
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const listing = new ProductList("Tents", dataSource, element);
 
-listing.init();
+const ProductData = new ProductData("tents");
