95d99eb60a74d3c772e87fd4a820a9c463b6a12d
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-05-10 12:29:51 -0500

    updating cart js

--- src/js/cart.js
+++ src/js/cart.js
@@ -26,4 +26,10 @@
 //   return newItem;
 // }
 
-renderCartContents();
+import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import ShoppingCart from "./ShoppingCart.mjs";
+
+loadHeaderFooter();
+
+const cart = new ShoppingCart("so-cart", ".product-list");
+cart.renderCartContents();
