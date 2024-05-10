606143923b31b49cd27c638e87883585e67ee443
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-05-10 16:45:55 -0500

    updating assignments week 3. Individual and task

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -1,10 +1,13 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function cartItemTemplate(item) {
+
+
+
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
+      src="${item.Images.PrimarySmall}"
       alt="${item.Name}"
     />
   </a>
@@ -14,7 +17,10 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+
 </li>`;
+
+console.log(item);
 
   return newItem;
 }
@@ -28,5 +34,26 @@ export default class ShoppingCart {
     const cartItems = getLocalStorage(this.key);
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");
+
+    if (cartItems.length > 0) {
+      const total = calculateTotal(cartItems);
+      showTotal(total);
+    }    
   }
 }
+
+function calculateTotal(cartItems) {
+  let total = 0;
+  for (const item of cartItems) {
+  total += item.FinalPrice;
+  }
+  return total.toFixed(2); // Round the total to two decimal places
+}
+
+function showTotal(total) {
+  const cartFooter = document.querySelector('.cart-footer');
+  cartFooter.classList.remove('hide'); // Show the total if there are items in the cart
+  const cartTotalElement = document.querySelector('.cart-total');
+  cartTotalElement.textContent = `Total: $${total}`;
+}
+  
---
+++ src/js/newsletter.js
@@ -0,0 +1,7 @@
+console.log("Newsletter script loaded");
+document.getElementById("newsletter-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const email = document.getElementById("email").value;
+
+    alert("Thank you for subscribing!");
+});
